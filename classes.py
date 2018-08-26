bbdcfe13dd28440d0095822bab82c317a29f555b
Shannen Jakosalem <z5164261@ad.unsw.edu.au>
2018-08-26 17:27:26 +1000

    added tests

--- classes.py
+++ classes.py
@@ -35,7 +35,7 @@ class Car(ABC):
     @abstractmethod
     def get_price(self):
         pass           
-
+        
 class Customer():
     def __init__(self, name, age, licence_number, email, credit_card):
         self._name = name
@@ -54,21 +54,23 @@ class Rental():
         # LHS class attributes
         # RHS initial values (passed through constructor)
     
-    def book_car(self, car, customer, rental_period, insurance, net_price,
+    def book_car(self, customer, car, rental_period, insurance, net_price,
                  pickup_location, drop_off_location):
-        self._car = car
-        self._customer = customer
-        self._rental_period = rental_period
-        self._insurance = insurance
-        self._net_price = car.get_price(self._rental_period)
-        self._pickup_location = pickup_location
-        self._drop_off_location = drop_off_location
+        print('BOOKING CONFIRMATION')
+        print('Customer Name: ' + customer2._name)
+        print('Car: ' + car3._make + ' ' + car3._model)
+        print('Rental period: ' + str(rental2._rental_period) + ' days')
+        print('Insurance: ' + str(rental2._insurance))
+        print('Net price: ' + str(car2.get_price(self._rental_period)))
+        print('Pickup location: ' + str(rental2._pickup_location))
+        print('Dropoff location: ' + str(rental2._drop_off_location))
 
 class SmallCar(Car):
     def get_price(self, rental):
         return rental * self._price_per_day
             
 class MediumCar(Car):
+
     def get_price(self, rental):
         return rental * self._price_per_day
 
@@ -100,13 +102,17 @@ class Manager():
 
 # TESTS !!!
 
-# CAR DETAILS - test for every type of car
+# CAR DETAILS - test for every type of car. Details called from SmallCar,
+# MediumCar, LargeCar and PremiumCar since parent Car class instantiable.
+# Tests located here for efficiency (they would have to be in all classes
+# listed above if they weren't here)
 car1 = SmallCar(7, 100, 'Toyota', 'Yaris', 2003)
 car2 = MediumCar(2, 3, 'Toyota', 'Camry', 2008)
 car3 = LargeCar(10, 56, 'Jeep', 'Cherokee', 2010)
-car4 = PremiumCar(1, 7, 'Range Rover', '???', 2005)
+car4 = PremiumCar(1, 7, 'Range Rover', 'Big Car', 2005)
 
-''' 
+# Replace car1 with 2, 3, 4 to test
+'''
 print('CAR DETAILS')
 print('Age: {}'.format(car1._age))
 print('Price per day: {}'.format(car1._price_per_day))
@@ -115,21 +121,28 @@ print('Model: {}'.format(car1._model))
 print('Year: {}'.format(car1._year))
 '''
 
-# RENTAL DETAILS - test for every type of car too
-customer1 = Customer('Tai Lopez', 41, 6771920, 'hereinmygarage@yahoo.com', 
-                     'Calabasas')
-rental1 = Rental(50, 'Yes', 'Central', 'Town Hall')
-test1 = rental1.book_car(car1, customer1._name, rental1._rental_period, 1, 1,
-                         rental1._pickup_location, rental1._drop_off_location)
+# CUSTOMER DETAILS - testing if customer details go through 
+customer1 = Customer('Tai Lopez', 41, 6771920,'hereinmygarage@yahoo.com',
+                     54701928493815732)
+customer2 = Customer('Katrina Davis', 89, 5728508, 'wowemail@hotmail.com',
+                           47195284620917492)
+customer3 = Customer('Myfirstname Andlastname', 17, 4820682,
+                     'coolies@wow.com', 38105729572840275)
 
-print('{}'.format(test1._customer))
-
-
+'''                 
+print('CUSTOMER DETAILS')
+print('Name: ' + customer1._name)
+print('Age: ' + str(customer1._age))
+print('Licence number: ' + str(customer1._licence_number))
+print('Email: ' + customer1._email)
+print('Credit card: ' + str(customer1._credit_card))
 '''
-print('RENTAL DETAILS')
-print('Car: {}'.format(test1._car))
-print('Customer: {}'.format(test1._customer))
-print('Rental period: {}'.format(test1._rental_period))
-print('Insurance: {}'.format(test1._insurance))
-print('Net price: {}'.format(test1._net_price))
-'''
+
+# RENTAL DETAILS - have to change values in book_car for details to print
+# out properly
+#rental1 = Rental(14, False, 'Sydney', 'Town Hall')
+#rental1.book_car(customer1, car1, rental1, rental1, car1, rental1, rental1)
+#rental2 = Rental(3, True, 'Bondi', 'Manly')
+#rental2.book_car(customer2, car3, rental2, rental2, car3, rental2, rental2)
+#rental3 = Rental(93, False, 'Sydney', Los Angeles')
+#rental3.book_car(cistomer3, car4, rental3, rental3, car4, rental3, rental3)
